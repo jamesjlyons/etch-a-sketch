15db379d3480b1832a5bfa96a80b1c6c358eabd6
James Lyons <jamesjlyons29@gmail.com>
2020-11-16 22:26:24 -0800

    working etch and clear

--- etch.js
+++ etch.js
@@ -1,7 +1,9 @@
 // Select elements on the page
 const canvas = document.querySelector("#etch");
 const ctx = canvas.getContext("2d");
-const shakeButton = document.querySelector(".shake");
+const shakeButton = document.querySelector(".shake-button");
+const moveAmount = 8;
+let hue = 0;
 
 // setup canvas
 // const width = canvas.width;
@@ -12,8 +14,12 @@ let y = Math.floor(Math.random() * height);
 
 ctx.lineJoin = "square";
 ctx.lineCap = "square";
-ctx.lineWidth = 8;
-ctx.strokeStyle = "#ffffff";
+ctx.lineWidth = moveAmount;
+// ctx.strokeStyle = "hsla(165, 82%, 52%, 1)";
+// ctx.shadowColor = "hsla(165, 82%, 52%, 0.8)";
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+ctx.shadowColor = `hsla(${hue}, 100%, 55%, 0.8)`;
+ctx.shadowBlur = 16;
 
 ctx.beginPath(); // start drawing
 ctx.moveTo(x, y);
@@ -21,9 +27,96 @@ ctx.lineTo(x, y);
 ctx.stroke();
 
 // draw function
+function draw({ key }) {
+  console.log(key);
+  hue += 4;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.shadowColor = `hsla(${hue}, 100%, 55%, 0.8)`;
+  // start path
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  // move x and y based on input
+  switch (key) {
+    case "ArrowUp":
+      y = y - moveAmount;
+      break;
+    case "ArrowDown":
+      y = y + moveAmount;
+      break;
+    case "ArrowRight":
+      x = x + moveAmount;
+      break;
+    case "ArrowLeft":
+      x = x - moveAmount;
+      break;
+    default:
+      break;
+  }
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
 
-// keypress handler
+function drawMore({ key }) {
+  console.log(key);
+  // start path
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  // move x and y based on input
+  switch (key) {
+    case "ArrowUp":
+      y = y - 24;
+      break;
+    case "ArrowDown":
+      y = y + 24;
+      break;
+    case "ArrowRight":
+      x = x + 24;
+      break;
+    case "ArrowLeft":
+      x = x - 24;
+      break;
+    default:
+      break;
+  }
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
+
+// keypress handlers
+function handleKey(e) {
+  if (e.key.includes("Arrow")) {
+    e.preventDefault();
+    draw({ key: e.key });
+  } else if (e.ShiftKey && e.key.includes("Arrow")) {
+    e.preventDefault();
+    drawMore({ key: e.key });
+  }
+}
 
 // clear function
+function clearCanvas() {
+  canvas.classList.add("shake");
+  fadeCanvas();
+  canvas.addEventListener(
+    "animationend",
+    function () {
+      canvas.classList.remove("shake");
+    },
+    { once: true }
+  );
+}
+function fadeCanvas() {
+  ctx.clearRect(0, 0, width, height);
+}
 
 // arrow key listener
+window.addEventListener("keydown", handleKey);
+
+// shakeButton listener
+shakeButton.addEventListener("click", clearCanvas);
+
+// arrow button listeners
+document.querySelector(".leftkey").addEventListener("click", arrowKeyHandler);
+document.querySelector(".upkey").addEventListener("click", arrowKeyHandler);
+document.querySelector(".downkey").addEventListener("click", arrowKeyHandler);
+document.querySelector(".rightkey").addEventListener("click", arrowKeyHandler);
